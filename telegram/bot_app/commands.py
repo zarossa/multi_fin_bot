57cf301fea624afe9f8f94bda297f3da5b666635
zarossa <mr.kirarik@yandex.ru>
2023-05-05 01:47:50 +0700

    fix-moves_functions

--- telegram/bot_app/commands.py
+++ telegram/bot_app/commands.py
@@ -1,13 +1,12 @@
 import os
 
-import aiohttp
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from aiogram.types import User
 
 from . import messages
 
 from .app import dp
+from .data_fetcher import login_user, register_user
 from .states import WorkStates
 
 USER_API_REGISTER = os.getenv('USER_API_REGISTER')
@@ -15,42 +14,19 @@ USER_API_LOGIN = os.getenv('USER_API_LOGIN')
 PASSWORD = os.getenv('PASSWORD')
 
 
-async def login_user(username: int, password: str = PASSWORD):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(USER_API_LOGIN, json={
-            'username': username,
-            'password': password
-        }) as response:
-            data = await response.json()
-            return data.get('auth_token')
-
-
-async def register_user(telegram_user: User, password: str = PASSWORD):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(USER_API_REGISTER, json={
-            'username': telegram_user.id,
-            'first_name': telegram_user.first_name,
-            'password': password
-        }) as response:
-            if response.status == 201:
-                data = await response.json()
-                # Process the response data if needed
-                return messages.REGISTER
-            else:
-                # Handle the error case
-                return 'Error creating user'
-
-
 @dp.message_handler(commands=['start', 'help'], state='*')
 async def send_welcome(message: types.Message, state: FSMContext):
     await WorkStates.start.set()
 
-    token = await login_user(username=message.from_user.id)
+    token = await login_user(message.from_user)
 
-    if not token:
-        answer = await register_user(message.from_user)
-        await message.reply(answer)
-        token = await login_user(username=message.from_user.id)
+    if not token.get('auth_token'):
+        is_registered = await register_user(message.from_user)
+        if not is_registered:
+            await message.reply(messages.ERROR)
+            return
+        await message.reply(messages.REGISTER)
+        token = await login_user(message.from_user)
     async with state.proxy() as data:
-        data['token'] = token
+        data['token'] = token.get('auth_token')
     await message.reply(messages.WELCOME_MESSAGE)
--- telegram/bot_app/data_fetcher.py
+++ telegram/bot_app/data_fetcher.py
@@ -1,19 +1,42 @@
 import aiohttp
 import os
 
-WORDS_API_URL_RANDOM = os.getenv('WORDS_API_URL_RANDOM')
-WORDS_API_URL_ALL = os.getenv('WORDS_API_URL_ALL')
+from aiogram.types import User
+
+USER_API_REGISTER = os.getenv('USER_API_REGISTER')
+USER_API_LOGIN = os.getenv('USER_API_LOGIN')
+CURRENCY_API_URL = os.getenv('CURRENCY_API_URL')
+PASSWORD = os.getenv('PASSWORD')
 
 
-async def get_random():
+async def register_user(telegram_user: User) -> dict | None:
     async with aiohttp.ClientSession() as session:
-        async with session.get(WORDS_API_URL_RANDOM) as response:
+        async with session.post(USER_API_REGISTER, json={
+            'username': telegram_user.id,
+            'first_name': telegram_user.first_name,
+            'password': PASSWORD
+        }) as response:
+            status = response.status
+            if status == 201:
+                return await response.json()
+            return None
+
+
+async def login_user(telegram_user: User) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(USER_API_LOGIN, json={
+            'username': telegram_user.id,
+            'password': PASSWORD
+        }) as response:
             return await response.json()
 
 
-async def get_next(pk):
+async def put_currency(token: str, currency: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'{WORDS_API_URL_ALL}{str(pk)}') as response:
+        async with session.put(
+                CURRENCY_API_URL,
+                headers={'Authorization': f'Token {token}'},
+                json={'currency_code': currency}) as response:
             status = response.status
             if status == 200:
                 return await response.json()
