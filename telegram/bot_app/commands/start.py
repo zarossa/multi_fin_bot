cb2fede2224fc4df9da23467309baeacee6cb92f
zarossa <mr.kirarik@yandex.ru>
2023-05-09 13:56:10 +0700

    fix-telegram-recreate_keyboard_to_function

--- telegram/bot_app/commands/start.py
+++ telegram/bot_app/commands/start.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher import FSMContext
 from .. import messages
 from ..app import dp, bot
 from ..data_fetcher import Account
-from ..keyboards import currency_kb
+from ..keyboards import keyboard_from_list
 from ..states import BaseStates, AccountStates
 
 
@@ -20,7 +20,8 @@ async def start(message: types.Message, state: FSMContext):
             data['token'] = account.token
         await message.answer(messages.WELCOME_MESSAGE)
     else:
-        await message.answer(text=messages.CURRENCY, reply_markup=currency_kb)
+        keyboard = await keyboard_from_list(['USD', 'RUB', 'KZT'])
+        await message.answer(text=messages.CURRENCY, reply_markup=keyboard)
         await AccountStates.create.set()
         async with state.proxy() as data:
             data['account'] = account
@@ -47,4 +48,5 @@ async def account_creating(callback_query: types.CallbackQuery, state: FSMContex
 @dp.callback_query_handler(state=AccountStates)
 async def process_invalid_currency_selection(callback_query: types.CallbackQuery):
     message = f"Please select a valid currency"
-    await bot.send_message(chat_id=callback_query.from_user.id, text=message, reply_markup=currency_kb)
+    keyboard = await keyboard_from_list(['USD', 'RUB', 'KZT'])
+    await bot.send_message(chat_id=callback_query.from_user.id, text=message, reply_markup=keyboard)
--- telegram/bot_app/keyboards.py
+++ telegram/bot_app/keyboards.py
@@ -1,15 +1,16 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
-currencies = ['USD', 'RUB', 'KZT']
-currency_kb = InlineKeyboardMarkup()
 
-[currency_kb.add(InlineKeyboardButton(currency, callback_data=currency)) for currency in currencies]
-
-
-async def category_keyboard(is_blank: bool = False):
+async def category_keyboard(is_blank: bool = False) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup()
     buttons = ['Create new']
     if not is_blank:
         buttons.extend(['Delete', 'Edit'])
+    [keyboard.add(InlineKeyboardButton(button, callback_data=button)) for button in buttons]
+    return keyboard
+
+
+async def keyboard_from_list(buttons: list) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup()
     [keyboard.add(InlineKeyboardButton(button, callback_data=button)) for button in buttons]
     return keyboard
