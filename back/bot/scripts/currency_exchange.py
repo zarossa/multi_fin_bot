2049b1f4444f8cabeae80924b916318efaa4c260
zarossa <mr.kirarik@yandex.ru>
2023-05-11 10:07:58 +0700

    add-back-THB_currency

--- back/bot/scripts/currency_exchange.py
+++ back/bot/scripts/currency_exchange.py
@@ -6,6 +6,7 @@ def convert(amount, user_currency: int, amount_currency: int) -> decimal.Decimal
         1: 1,
         2: 76,
         3: 440,
+        4: 34,
     }
     rate = exchange_rates.get(user_currency) / exchange_rates.get(amount_currency)
     return decimal.Decimal(float(amount) * rate)
