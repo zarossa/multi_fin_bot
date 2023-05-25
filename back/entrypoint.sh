c678615d5546b3797fda9709c9ced8034d41487a
zarossa <mr.kirarik@yandex.ru>
2023-05-25 23:22:57 +0700

    update_nginx&docker_files

---
+++ back/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn proj.wsgi:application --bind 0.0.0.0:8000
--- back/proj/settings.py
+++ back/proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('HOST'), ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
