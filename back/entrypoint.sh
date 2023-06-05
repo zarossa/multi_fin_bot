ee87ea04694ecab090950eb52538a8516f2c849c
Yaroslav Zakhvatkin <97903006+zarossa@users.noreply.github.com>
2023-06-05 10:40:02 +0700

    add-celery_start_in_entrypoint

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn proj.wsgi:application --bind 0.0.0.0:8000
+gunicorn proj.wsgi:application --bind 0.0.0.0:8000 &
+
+celery -A proj worker -l INFO &
+
+celery -A proj beat -l INFO
